b3b52aadc7f92e74a101beae5463cc61430ac296
verakorenblik <verakorenblik@live.nl>
2026-04-23 14:51:19 +0200

    Update analysis and presentation

--- load_csvs.R
+++
@@ -1,3 +0,0 @@
-information <- read.csv2(here::here("test_information_2026.csv"))
-responses <- read.csv2(here::here("responses_2026.csv"))
-person <- read.csv2(here::here("person_information_2026.csv"))
